5704d1ebba4833da51ea8c4be73552fc92d4b7f1
pimtegelaar <pimtegelaar@gmail.com>
2015-04-19 23:41:18 +0200

    Solution for beer card emulator

---
+++ solutions/BarrysBeerBarCard/app/src/main/java/com/barrysbeerbar/card/MainActivity.java
@@ -0,0 +1,38 @@
+package com.barrysbeerbar.card;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.card_account_field);
+        editText.setText(CardStorage.getCardHolderName(this));
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                CardStorage.setCardHolderName(MainActivity.this, s.toString());
+            }
+        });
+    }
+
+}
